00903abeb7463f2a616b8f257e111587e69479d4
Luis Henrique Tetsuo Abeno de Mambro <luis.mambro@onr.org.br>
2024-05-04 22:08:29 -0300

    fix: fixing load

--- script.js
+++ script.js
@@ -22,59 +22,61 @@ backgroundLayer4.src = "./background-layers/layer-4.png";
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = "./background-layers/layer-5.png";
 
-const slider = document.getElementById("slider");
-slider.value = gameSpeed;
+window.addEventListener("load", () => {
+  const slider = document.getElementById("slider");
+  slider.value = gameSpeed;
 
-const showGameSpeed = document.getElementById("showGameSpeed");
-showGameSpeed.innerHTML = gameSpeed;
+  const showGameSpeed = document.getElementById("showGameSpeed");
+  showGameSpeed.innerHTML = gameSpeed;
 
-slider.oninput = (e) => {
-  gameSpeed = e.target.value;
-  showGameSpeed.innerHTML = e.target.value;
-};
+  slider.oninput = (e) => {
+    gameSpeed = e.target.value;
+    showGameSpeed.innerHTML = e.target.value;
+  };
 
-class Layer {
-  constructor(image, speedModifier) {
-    this.x = 0;
-    this.y = 0;
-    this.width = 2400;
-    this.height = 700;
-    this.image = image;
-    this.speedModifier = speedModifier;
-    this.speed = gameSpeed * this.speedModifier;
-  }
-
-  update() {
-    this.speed = gameSpeed * this.speedModifier;
-    this.x -= this.speed;
-    if (this.x < -this.width) {
+  class Layer {
+    constructor(image, speedModifier) {
       this.x = 0;
+      this.y = 0;
+      this.width = 2400;
+      this.height = 700;
+      this.image = image;
+      this.speedModifier = speedModifier;
+      this.speed = gameSpeed * this.speedModifier;
+    }
+
+    update() {
+      this.speed = gameSpeed * this.speedModifier;
+      this.x -= this.speed;
+      if (this.x < -this.width) {
+        this.x = 0;
+      }
+    }
+
+    draw() {
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+      ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
     }
   }
 
-  draw() {
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
-  }
-}
+  const layer1 = new Layer(backgroundLayer1, 0.2);
+  const layer2 = new Layer(backgroundLayer2, 0.4);
+  const layer3 = new Layer(backgroundLayer3, 0.6);
+  const layer4 = new Layer(backgroundLayer4, 0.8);
+  const layer5 = new Layer(backgroundLayer5, 1);
 
-const layer1 = new Layer(backgroundLayer1, 0.2);
-const layer2 = new Layer(backgroundLayer2, 0.4);
-const layer3 = new Layer(backgroundLayer3, 0.6);
-const layer4 = new Layer(backgroundLayer4, 0.8);
-const layer5 = new Layer(backgroundLayer5, 1);
+  const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
-const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+  const animate = () => {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-const animate = () => {
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    gameObjects.forEach((object) => {
+      object.update();
+      object.draw();
+    });
+    gameFrame--;
+    requestAnimationFrame(animate);
+  };
 
-  gameObjects.forEach((object) => {
-    object.update();
-    object.draw();
-  });
-  gameFrame--;
-  requestAnimationFrame(animate);
-};
-
-animate();
+  animate();
+});
